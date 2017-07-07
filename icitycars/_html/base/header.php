3e92da7f940a36e2f306a41601e3239abf16c475
StrongerMyself <misha.smerch.1997@gmail.com>
2017-07-07 23:49:57 +0800

    add site icitycars

---
+++ icitycars/_html/base/header.php
@@ -0,0 +1,25 @@
+<div class="h">
+	<div class="h__wrap wrap">
+		<div class="h__burger burger"><i></i></div>
+		<a href="#" class="h__logo logo"></a>
+		<a href="#" class="h__login"></a>
+	</div>
+</div>
+
+<div class="nav">
+	<a href="#" class="logo"></a>
+	<ul>
+		<li><a href="#">Главная</a></li>
+		<li><a href="#">Встречи</a></li>
+		<li><a href="#">Тачки</a></li>
+		<li><a href="#">Новости</a></li>
+		<li><a href="#">О нас</a></li>
+		<li><a href="#">Условия участия</a></li>
+		<li><a href="#">Контакты</a></li>
+	</ul>
+	<div class="soc">
+		<a href="#" class="iconic iconic--vk"></a>
+		<a href="#" class="iconic iconic--youtube"></a>
+		<a href="#" class="iconic iconic--insta"></a>
+	</div>
+</div>
